bb5ece1013c1f48f5e29e8096799e47fc193c4ea
Irving Gonzalez <irvingemmanuelgg@gmail.com>
2019-04-06 13:49:26 -0600

    Agregar bootstrap
    Componentes basicos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
