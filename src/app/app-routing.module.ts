38cd634843e9507ab765d7ba4c69e1ccef35b0ee
Irving Gonzalez <irvingemmanuelgg@gmail.com>
2019-04-10 20:17:56 -0500

    Componentes y rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,67 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AboutComponent } from './pages/about/about.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { CartComponent } from './pages/cart/cart.component';
+import { CombosComponent } from './pages/combos/combos.component';
+import { CreateUserComponent } from './pages/create-user/create-user.component';
+import { FaqComponent } from './pages/faq/faq.component';
+import { FeaturedProductsComponent } from './pages/featured-products/featured-products.component';
 import { HomeComponent } from './pages/home/home.component';
+import { NewProductsComponent } from './pages/new-products/new-products.component';
+import { ShopComponent } from './pages/shop/shop.component';
+import { TournamentsComponent } from './pages/tournaments/tournaments.component';
+import { TrophiesComponent } from './pages/trophies/trophies.component';
 
 const routes: Routes = [
   {
+    path: 'acerca',
+    component: AboutComponent
+  },
+  {
+    path: 'blog',
+    component: BlogComponent
+  },
+  {
+    path: 'carrito',
+    component: CartComponent
+  },
+  {
+    path: 'combos',
+    component: CombosComponent
+  },
+  {
+    path: 'crear-usuario',
+    component: CreateUserComponent
+  },
+  {
+    path: 'faq',
+    component: FaqComponent
+  },
+  {
+    path: 'productos-destacados',
+    component: FeaturedProductsComponent
+  },
+  {
     path: 'home',
     component: HomeComponent
+  },
+  {
+    path: 'productos-nuevos',
+    component: NewProductsComponent
+  },
+  {
+    path: 'tienda',
+    component: ShopComponent
+  },
+  {
+    path: 'torneos',
+    component: TournamentsComponent
+  },
+  {
+    path: 'trofeos',
+    component: TrophiesComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,35 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { CreateUserComponent } from './pages/create-user/create-user.component';
+import { CartComponent } from './pages/cart/cart.component';
+import { ShopComponent } from './pages/shop/shop.component';
+import { CombosComponent } from './pages/combos/combos.component';
+import { NewProductsComponent } from './pages/new-products/new-products.component';
+import { FeaturedProductsComponent } from './pages/featured-products/featured-products.component';
+import { TrophiesComponent } from './pages/trophies/trophies.component';
+import { AboutComponent } from './pages/about/about.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { TournamentsComponent } from './pages/tournaments/tournaments.component';
+import { FaqComponent } from './pages/faq/faq.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     FooterComponent,
-    NavbarComponent
+    NavbarComponent,
+    CreateUserComponent,
+    CartComponent,
+    ShopComponent,
+    CombosComponent,
+    NewProductsComponent,
+    FeaturedProductsComponent,
+    TrophiesComponent,
+    AboutComponent,
+    BlogComponent,
+    TournamentsComponent,
+    FaqComponent
   ],
   imports: [
     BrowserModule,
