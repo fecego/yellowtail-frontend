129a5ae276f9afd0ac478b77b21ba5225ad05486
ShantalExperimenta <shantal.govantes@experimenta.com.mx>
2019-04-10 19:57:11 -0500

    carrousel

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './pages/home/home.component';
+import { CarrouselComponent } from './components/carrousel/carrousel.component';
 
 const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent
+  },
+  {
+    path: 'carrousel',
+    component:CarrouselComponent
   }
 ];
 
