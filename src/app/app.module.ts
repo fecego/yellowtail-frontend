9cb85c813097033da0da11d173c4c39d4ddfb277
Irving Gonzalez <irvingemmanuelgg@gmail.com>
2019-05-01 11:40:52 -0500

    Funcionamiento de FAQs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule }   from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -62,7 +63,8 @@ import { ProfileComponent } from './pages/profile/profile.component';
     BrowserModule,
     AppRoutingModule,
     NgbModule.forRoot(),
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/pages/faq/faq.component.ts
+++ src/app/pages/faq/faq.component.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, transition, animate, style } from '@angular/animations'
+
+import data from './questions.json';
 
 @Component({
   selector: 'app-faq',
   templateUrl: './faq.component.html',
-  styleUrls: ['./faq.component.css']
+  styleUrls: ['./faq.component.css'],
+  animations: [
+    trigger('slideInOut', [
+      transition(':enter', [
+        style({transform: 'translateY(-100%)'}),
+        animate('200ms ease-in', style({transform: 'translateY(0%)'}))
+      ]),
+      transition(':leave', [
+        animate('200ms ease-in', style({transform: 'translateY(-100%)'}))
+      ])
+    ])
+  ]
 })
 export class FaqComponent implements OnInit {
 
-  constructor() { }
+  show: Boolean;
+  faqs: Array<any>;
+
+  constructor() {
+    this.show = false;
+    const renderFaqs = data.faqs.map((faq, index) => {
+      faq.index = index;
+      faq.show = false;
+      return faq;
+    });
+    this.faqs = renderFaqs;
+    console.log(this.faqs);
+  }
 
   ngOnInit() {
+
+  }
+
+  toggle(index) {
+    console.log(index);
+    const faq = this.faqs[index];
+    faq.show = !faq.show;
+    this.faqs[index] = faq;
   }
 
 }
