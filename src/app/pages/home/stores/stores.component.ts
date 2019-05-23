1ca4d4fe34b68fcea2c2c3101e1957b8adbd746c
Irving Gonzalez <irvingemmanuelgg@gmail.com>
2019-05-23 12:35:52 -0500

    Mapa de tienda fisica

--- src/app/pages/home/stores/stores.component.ts
+++ src/app/pages/home/stores/stores.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+/// <reference types="@types/googlemaps" />
 
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {} from 'googlemaps';
 @Component({
   selector: 'app-stores',
   templateUrl: './stores.component.html',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StoresComponent implements OnInit {
 
+  @ViewChild('gmap') gmapElement: any;
+  map: google.maps.Map;
+
   constructor() { }
 
   ngOnInit() {
+    const mapProp = {
+      center: new google.maps.LatLng(18.5793, 73.8143),
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
   }
 
 }
