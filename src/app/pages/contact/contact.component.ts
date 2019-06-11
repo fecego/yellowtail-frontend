023aa3c7ed2d96cd4da94b3f00db56e881f5edea
LindaLopezR <guadalupelopez.lr@gmail.com>
2019-06-11 16:01:05 -0500

    Vista de contacto
    Enlace a contacto desde el footer
    
    Corección de título en sección de tiendas

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {} from 'googlemaps';
 
 @Component({
   selector: 'app-contact',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
+  @ViewChild('gmap') gmapElement: any;
+  map: google.maps.Map;
+
+  @ViewChild('mmap') mmapElement: any;
+  mmap: google.maps.Map;
 
   constructor() { }
 
   ngOnInit() {
+    const mapProp = {
+      center: new google.maps.LatLng(18.5793, 73.8143),
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
+    this.mmap = new google.maps.Map(this.mmapElement.nativeElement, mapProp);
   }
 
 }
