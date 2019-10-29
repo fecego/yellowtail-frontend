47c66e39d605fa04815130be9c6676eba12d252b
LindaLopezR <guadalupelopez.lr@gmail.com>
2019-10-28 20:30:11 -0600

    Efecto parallax

--- src/app/pages/home/featured-products/featured-products.component.ts
+++ src/app/pages/home/featured-products/featured-products.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import * as Parallax from 'parallax-js';
+
+declare var Parallax: any;
 
 @Component({
   selector: 'app-featured-products',
@@ -10,6 +13,21 @@ export class FeaturedProductsComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterContentInit() {
+    const scene = document.getElementById('scene');
+    const scene2 = document.getElementById('scene2');
+    const scene3 = document.getElementById('scene3');
+    const parallaxInstance = new Parallax(scene, {
+      relativeInput: true
+    });
+    const parallaxInstance2 = new Parallax(scene2, {
+      relativeInput: true
+    });
+    const parallaxInstance3 = new Parallax(scene3, {
+      relativeInput: true
+    });
   }
 
 }
